93225b08fb1119443821ee610b8fb06128678364
Luigi Mendoza <luigi.mendoza@bit2bitamericas.com>
2021-08-27 20:59:52 -0500

    first commit

---
+++ HelloTesting20212/HelloTesting20212/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTesting20212
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
